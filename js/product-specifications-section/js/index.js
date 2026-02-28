fa05e31927d09a413901445db964ccb78b21ff22
dallana <cindydallana2822@gmail.com>
2026-02-28 16:36:26 -0500

    js: forEach funcional

--- js/product-specifications-section/js/index.js
+++ js/product-specifications-section/js/index.js
@@ -1,10 +1,29 @@
 const tabs = document.querySelectorAll('[role="tab"]');
 const panels = document.querySelectorAll('[role="tabpanel"]');
+const color = document.querySelectorAll("[aria-selected]");
 
-// 'tabs' es la lista de todos tus botones [role="tab"]
 tabs.forEach((tab) => {
     tab.addEventListener("click", () => {
-        // ¡Este código se ejecutará sin importar en qué pestaña hagas clic!
-        console.log("Hiciste clic en una pestaña");
+        const panelId = tab.getAttribute("aria-controls");
+        panels.forEach((panels) => {
+            panels.classList.add("hidden");
+        });
+        const elPanelQueQuiero = document.getElementById(panelId);
+        if (elPanelQueQuiero) {
+            elPanelQueQuiero.classList.remove("hidden");
+        }
+    });
+});
+
+color.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        const botonSeleccionado = tab.getAttribute("aria-selected");
+
+        color.forEach((tabs) => {
+            color.classList.add("hidden");
+        });
+        const activarBotones = document.getElementById(botonSeleccionado);
+        if (activarBotones) {
+        }
     });
 });
