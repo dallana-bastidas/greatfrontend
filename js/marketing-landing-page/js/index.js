867a255b34cdb51f327619c6f5dfe7042009e9e8
dallana <cindydallana2822@gmail.com>
2026-03-09 19:27:18 -0500

    js(marketing-landing-page): navbar implementando componentes

--- js/marketing-landing-page/js/index.js
+++ js/marketing-landing-page/js/index.js
@@ -1,2 +1,45 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const btnHeader = [
+    {
+        text: "Learn more",
+        class: "rounded-md relative p-2 bg-white text-neutral-950 hover:bg-gray-200",
+    },
+    {
+        text: "See pricing",
+        class: "rounded-md p-2 bg-indigo-700 text-white hover:bg-indigo-500",
+    },
+];
+
+const btnFooter = [
+    {
+        text: "Monthly",
+        class: "",
+    },
+    {
+        text: "Annually",
+        class: "",
+    },
+    {
+        text: "Buy now",
+        class: "",
+    },
+];
+
+function crearBotones(listaDeBotones = [], idDestino) {
+    const contenedor = document.getElementById(idDestino);
+
+    if (!contenedor) {
+        console.error(`No existe un elemento con el id: ${idDestino}`);
+        return;
+    }
+
+    contenedor.innerHTML = "";
+
+    listaDeBotones.forEach((botonInfo) => {
+        const btn = document.createElement("button");
+        btn.textContent = botonInfo.text;
+        btn.className = `rounded-md p-2 ${botonInfo.class}`;
+        contenedor.append(btn);
+    });
+}
+
+crearBotones(btnHeader, "btnNav");
