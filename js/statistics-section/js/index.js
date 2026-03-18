bd686da8d16bb40131192710caa0030b18521c3c
dallana <cindydallana2822@gmail.com>
2026-03-17 19:20:52 -0500

    js: diseño
    - se agrego titulo
    - descripcion
    - imagen

--- js/statistics-section/js/index.js
+++ js/statistics-section/js/index.js
@@ -1,2 +1,7 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+fetch(
+    "https://www.greatfrontend.com/api/projects/challenges/statistics-metrics",
+)
+    .then((response) => response.JSON())
+    .then((data) => {
+        console.log(data);
+    });
