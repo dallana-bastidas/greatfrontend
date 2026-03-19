45f32c3b8c1098974f9989963b062b2bb3f8e6d5
dallana <cindydallana2822@gmail.com>
2026-03-18 20:07:29 -0500

    js: conexion con la appi y diseño terminado

--- js/statistics-section/js/index.js
+++ js/statistics-section/js/index.js
@@ -1,7 +1,45 @@
 fetch(
     "https://www.greatfrontend.com/api/projects/challenges/statistics-metrics",
 )
-    .then((response) => response.JSON())
+    .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+        crearCard(data.data);
     });
+
+function crearCard(infoApi) {
+    const info = document.getElementById("contenedor");
+
+    info.innerHTML = "";
+
+    infoApi.forEach((spanInfo) => {
+        const newDiv = document.createElement("div");
+        const newSpan = document.createElement("span");
+        const newTitulo = document.createElement("span");
+
+        newSpan.textContent = spanInfo.value.toLocaleString();
+        newTitulo.textContent = spanInfo.metric;
+
+        newDiv.appendChild(newSpan);
+        newDiv.appendChild(newTitulo);
+
+        newDiv.classList.add(
+            "border-2",
+            "border-gray-200",
+            "h-24",
+            "w-96",
+            "mt-6",
+            "rounded-md",
+            "flex",
+            "flex-col",
+            "justify-center",
+            "items-center",
+            "bg-white",
+        );
+
+        newSpan.classList.add("text-4xl", "font-bold", "text-indigo-700");
+
+        newTitulo.classList.add("text-sm", "text-gray-500");
+
+        info.appendChild(newDiv);
+    });
+}
