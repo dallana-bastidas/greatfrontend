28b82e6f5cdcc1d4e3e63cc97cdbbc82d20190f3
dallana <cindydallana2822@gmail.com>
2026-03-21 22:33:28 -0500

    js:agregue logica para cambiar el nombre del color
    -Cuando se da  que click en el color se mostrara el nombre del
    color seleccionado con ayuda del dataset

--- js/product-listing-section/js/index.js
+++ js/product-listing-section/js/index.js
@@ -2,51 +2,70 @@ async function obtenerInfo() {
     const url = new URL(
         "https://www.greatfrontend.com/api/projects/challenges/e-commerce/products",
     );
-    url.searchParams.set("per_page", "2");
+    url.searchParams.set("per_page", "9");
 
     try {
         const response = await fetch(url);
-
-        if (!response.ok) {
-            throw new Error(`Error HTTP detectado: ${response.status}`);
-        }
-
+        if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
         const data = await response.json();
-
         crearCard(data.data);
     } catch (error) {
-        console.error("No se pudo obtener la información:", error.message);
+        console.error("ERROR:", error.message);
     }
 }
 
+const contenedor = document.getElementById("contenedorProductos");
+
+contenedor.addEventListener("click", (event) => {
+    const colorSeleccionado = event.target.closest("[data-color]");
+
+    if (colorSeleccionado) {
+        //dataset es una interfaz  de js que permite acceder a todos los atributos personalizdos
+        const idDelProducto = colorSeleccionado.dataset.productId;
+
+        const targetElement = document.getElementById(
+            `color-text-${idDelProducto}`,
+        );
+
+        if (targetElement) {
+            targetElement.textContent = colorSeleccionado.dataset.color;
+        }
+    }
+});
+
 function crearCard(productos) {
-    const contenedor = document.getElementById("contenedorProductos");
+    const htmlCards = productos
+        .map((producto) => {
+            return `
+            <div class="card bg-white m-4 p-2 ">
+                <div class="flex justify-center items-center">
+                    <img src="${producto.images[0].image_url}" class=""/>
+                </div>
 
-    contenedor.innerHTML = "";
+                <p id="color-text-${producto.id}" class="text-gray-600">${producto.colors[0]}</p>
 
-    productos.forEach((producto) => {
-        console.log("Estructura del producto:", producto);
-        const cardHTML = `
-            <div class="card">
+                <h3 class="">${producto.name}</h3>
+                <span class="text-gray-600">$${producto.priceRange.highest}</span>
 
-            <div>
-            <img src="${producto.images[0].image_url}" class="size-64"/>
-            </div>
-            <p> </p>
-
-                <h3>${producto.name}</h3>
-                <span>$${producto.priceRange.highest}</span>
-                ${producto.colors
-                    .map((color) => {
-                        return `<span class="inline-block size-6 bg-${color}-500 bg-${color} rounded-full border"></span>`;
-                    })
-                    .join("")}
-
+                <div class="colors-container">
+                    ${producto.colors
+                        .map(
+                            (color) => `
+                        <span
+                            data-color="${color}"
+                            data-product-id="${producto.id}"
+                            class="inline-block size-6 bg-${color}-500 bg-${color} rounded-full border cursor-pointer">
+                        </span>
+                    `,
+                        )
+                        .join("")}
+                </div>
             </div>
         `;
+        })
+        .join("");
 
-        contenedor.innerHTML += cardHTML;
-    });
+    contenedor.innerHTML = htmlCards;
 }
 
 obtenerInfo();
