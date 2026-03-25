1e2f43a5d1a951acb96b2340f824283d0fb0e2b5
dallana <cindydallana2822@gmail.com>
2026-03-25 17:11:13 -0500

    js:hice responsive el default
    - Termine de darle responsive para mobile,desktop,tablet a la primera vista default
    
    Please enter the commit message for your changes. Lines starting

--- js/product-listing-section/js/index.js
+++ js/product-listing-section/js/index.js
@@ -2,7 +2,7 @@ async function obtenerInfo() {
     const url = new URL(
         "https://www.greatfrontend.com/api/projects/challenges/e-commerce/products",
     );
-    url.searchParams.set("per_page", "9");
+    url.searchParams.set("per_page", "2");
 
     try {
         const response = await fetch(url);
@@ -37,16 +37,15 @@ function crearCard(productos) {
     const htmlCards = productos
         .map((producto) => {
             return `
-            <div class="card bg-white m-4 p-2 ">
-                <div class="flex justify-center items-center">
-                    <img src="${producto.images[0].image_url}" class=""/>
+            <div class="flex  flex-col  card bg-white md:m-2 lg:m-2  ">
+                <div class="flex flex-1 justify-center items-center ">
+                    <img src="${producto.images[0].image_url}" class=" m-6 size-56  flex-1 object-cover min-w-0 rounded-md"/>
                 </div>
 
+                <div class="ml-6 flex flex-col gap-2">
                 <p id="color-text-${producto.id}" class="text-gray-600">${producto.colors[0]}</p>
-
                 <h3 class="">${producto.name}</h3>
                 <span class="text-gray-600">$${producto.priceRange.highest}</span>
-
                 <div class="colors-container">
                     ${producto.colors
                         .map(
@@ -60,6 +59,13 @@ function crearCard(productos) {
                         )
                         .join("")}
                 </div>
+                </div>
+
+
+
+
+
+
             </div>
         `;
         })
